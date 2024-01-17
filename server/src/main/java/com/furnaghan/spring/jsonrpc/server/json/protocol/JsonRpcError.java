ab07cdc3939438ed12f550bc24aef84520e25195
Jamie Furnaghan <jamie@fanduel.com>
2024-01-17 09:27:55 +0000

    Add unsupported operation error handling

--- server/src/main/java/com/furnaghan/spring/jsonrpc/server/json/protocol/JsonRpcError.java
+++ server/src/main/java/com/furnaghan/spring/jsonrpc/server/json/protocol/JsonRpcError.java
@@ -7,10 +7,15 @@ public record JsonRpcError(int code, String message, Object data) {
 	public static final int INVALID_PARAMS = -32602;
 	public static final int PARSE_ERROR = -32700;
 	public static final int UNKNOWN_ERROR = -32603;
+	public static final int UNSUPPORTED_OPERATION = -32604;
 
 	public static JsonRpcError from( final Throwable error ) {
-		if ( error instanceof RpcServerException rpc ) {
-			return new JsonRpcError( rpc.getCode(), rpc.getMessage(), rpc.getData() );
+		if ( error instanceof RpcServerException e ) {
+			return new JsonRpcError( e.getCode(), e.getMessage(), e.getData() );
+		}
+
+		if ( error instanceof UnsupportedOperationException e ) {
+			return new JsonRpcError( UNSUPPORTED_OPERATION, e.getMessage(), null );
 		}
 
 		return new JsonRpcError( UNKNOWN_ERROR, error.getMessage(), null );
